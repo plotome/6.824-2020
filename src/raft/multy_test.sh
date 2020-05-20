6f541a5bcbd527416f37e9460dc6f88852857279
tobe <1247363436@qq.com>
2020-05-21 07:40:06 +0800

    add support to run all tests

--- src/raft/multy_test.sh
+++ src/raft/multy_test.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-# eg: sh multy_test.sh 2B out 100
+# eg: sh multy_test.sh out 100 2B
+# eg: sh multy_test.sh out 100 (test all tests)
 set -e
-if [ $# -ne 3 ]; then
-	echo "Usage: $0 [test] [output file] [repeat time]"
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 [output file] [repeat time] [test name]"
 	exit 1
 fi
 
-# export "GOPATH=$(git rev-parse --show-toplevel)"
-# cd "${GOPATH}/src/raft"
-for i in `seq $3`
-do
-  echo "current round: $i"
-  echo "count: $i" > $2
-	go test -run $1 >> $2
-done
+if [ $# -eq 3 ]; then
+  for i in `seq $2`
+  do
+    echo "current round: $i"
+    echo "count: $i" > $1
+	  go test -run $3 >> $1
+  done
+fi
+
+if [ $# -eq 2 ]; then
+  for i in `seq $2`
+  do
+    echo "current round: $i"
+    echo "count: $i" > $1
+	  go test >> $1
+  done
+fi
 
 echo "no failure occur!"
